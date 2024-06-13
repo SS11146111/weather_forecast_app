dad48a63067b17ed8f242c1a7235f1dca614efaf
SS11146111 <sahasangita59@gmail.com>
2024-06-13 09:48:56 +0530

    weather forecast report 1.1

--- js/dateTime.js
+++ js/dateTime.js
@@ -131,4 +131,47 @@ function showPosition(position)
 }
 
 
+document.getElementById("enterForecast").addEventListener("click",
+    function(){
 
+        const cityForecast = document.getElementById("cityForecast").value;
+        const days = document.getElementById("days").value;
+        console.log(days)
+
+        fetch("http://api.weatherapi.com/v1/forecast.json?key=b6c462720ea9421a933195817241206&q="+cityForecast+"&days="+days+"&aqi=no&alerts=no")
+            .then(res => res.json())
+            .then(json => 
+                {
+                    /*document.getElementById("report").style.display = "block";
+
+                    document.getElementById("myCity").innerHTML = city.toUpperCase();
+
+                    let temperature = (json.main.temp - 273.15).toFixed(1);
+                    document.getElementById("temperature").innerHTML = temperature;
+                
+                    let wind = json.wind.speed.toFixed(1);
+                    document.getElementById("wind").innerHTML = wind;
+
+                    let humidity = json.main.humidity;
+                    document.getElementById("humidity").innerHTML = humidity;
+                    
+                    let visibility = json.visibility;
+                    document.getElementById("visibility").innerHTML = visibility;
+
+                    let src = json.weather[0].icon;
+                    let desc = json.weather[0].description;
+                    {
+                        let firstLetter = desc.charAt(0).toUpperCase();
+                        let remainingLetters = desc.substring(1);
+                        desc = firstLetter + remainingLetters;
+                    }
+                    
+                    document.getElementById("weatherCnd").src="https://openweathermap.org/img/wn/" + src +"@2x.png";
+                    document.getElementById("weatherDesc").innerHTML = desc;*/
+                    console.log(json.forecast)
+                
+                })
+    }
+    )
+
+   
